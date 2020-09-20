46ff3f46ea391e878aa07a9101af4352d52de82c
Anna Claire <acfields11@gmail.com>
2020-09-19 20:43:02 -0500

    update redirect

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 
   spotify: {
     clientId: "8c4aa610a98b41728a78698a1675fc80",
-    redirectUri: "https://franklovefrank.github.io/personal-website/yt2sp.html/callback/"
+    redirectUri: "https://franklovefrank.github.io/personal-website/yt2sp.html"
   },
 
   youtube: {
