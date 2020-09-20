94cb8b9c6c70b2ae232940c4563baeeecd8097cb
Anna Claire <acfields11@gmail.com>
2020-09-19 20:15:10 -0500

    new files

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+
+  spotify: {
+    clientId: "b92710d7bc9846309e7441a0c1e153fe",
+    redirectUri: "http://localhost:8888/GitHub/Spotify-to-Youtube/"
+  },
+
+  youtube: {
+    clientId: "145961692921-hm857au6q06qfcgltelv7rio3befhisj.apps.googleusercontent.com",
+    scope: 'https://gdata.youtube.com'
+  }
+
+};
