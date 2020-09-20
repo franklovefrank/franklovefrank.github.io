6d5306b64f1077e7c2cd1cf6b402b92eb31e0d9b
Anna Claire <acfields11@gmail.com>
2020-09-19 20:50:41 -0500

    update callback

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 
   spotify: {
     clientId: "8c4aa610a98b41728a78698a1675fc80",
-    redirectUri: "https://franklovefrank.github.io/personal-website/yt2sp.html"
+    redirectUri: 'http://localhost:8888/callback'
   },
 
   youtube: {
